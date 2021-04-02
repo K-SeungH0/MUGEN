13b4af0555dc3c6efe1175d3d359103ba3981365
NamSang <namssi94@gmail.com>
2021-04-02 10:58:59 +0900

    머지 푸시

--- MUGEN/Mugen.h
+++ MUGEN/Mugen.h
@@ -13,12 +13,5 @@
 extern HINSTANCE g_hInstance;
 extern HWND g_hWnd;
 
-extern bool isDebugMode;
-
 using namespace std;
 
-inline RECT GetRectOffset(POINTFLOAT pos, POINTFLOAT offset, int width, int height)
-{
-	RECT rect = { pos.x + offset.x, pos.y + offset.y,pos.x + offset.x + width, pos.y + offset.y + height };
-	return rect;
-}
